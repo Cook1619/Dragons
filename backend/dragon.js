ee2b65f7d78435341da433b4e53c2eb19180d761
Matt <cook6348@gmail.com>
2019-01-03 06:06:35 -0600

    Dynamically adding dragons even it default values arent set, and creating a dynamic date

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,8 +1,21 @@
-class Dragon {
-    constructor({birthdate, nickname}){
-        this.birthdate =  birthdate;
-        this.nickname = nickname;
+//The set default properties
+const DEFAULT_PROPERTIES = {
+    nickname: 'unnamed',
+    //Gets a date based on when the object is actually created, not when the file is being evaluated
+    get birthdate(){
+        return new Date();
     }
 }
 
+class Dragon {
+    //introducing in arguments in with {} ensures order doesnt mattter when creating a new instance of the object
+    //Setting a default object if the incoming one is undefined 
+    constructor({birthdate, nickname} = {}){
+        //Setting up properties for the dragon class
+        // || sets up default properties if known are given
+        this.birthdate =  birthdate || DEFAULT_PROPERTIES.birthdate;
+        this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+    }
+}
+//Exports the dragon class for use in another file
 module.exports = Dragon;
