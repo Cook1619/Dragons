240cc487be65d8c6136c99eb54d655fde22fa2c4
Matt <cook6348@gmail.com>
2019-01-04 07:48:12 -0600

    database set up

---
+++ backend/bin/configure_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Configuring dragonstachdb"
+
+dropdb -U node_user dragonstackdb
+createdb -U node_user dragonstackdb
+
+psql -U node_user dragonstackdb < ./bin/sql/generation.sql
+psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
+
+echo "dragonstackdb was configured"
